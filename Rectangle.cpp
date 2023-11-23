d6781c35a7db8a31a9e1ee2f7f5f7cbbae63cc44
TimingUP <1598240874@qq.com>
2023-11-23 13:40:23 +0800

    Achieve a single rectangular dynamic drawing function.

--- Rectangle.cpp
+++ Rectangle.cpp
@@ -8,8 +8,8 @@ void Rect::Draw()
 {
 
 	//设置剪辑区域
-	HRGN rect = CreateRectRgn(0, 0, 960, 540);
-	setcliprgn(rect);
+	//HRGN rect = CreateRectRgn(0, 0, 960, 540);
+	//setcliprgn(rect);
 
 	char s[10];
 	InputBox(s, 10, "请输入矩形左上角顶点横坐标","矩形","500", 500, 0, false);
@@ -26,10 +26,49 @@ void Rect::Draw()
 	LowerRight.y = UpperLeft.y + height;
 	line.setThickness(atof(s));
 	line.SetLineStyle();
-	
+	HRGN rect = CreateRectRgn(150, 0, 960, 540);
+	setcliprgn(rect);
+	int flag = 1;
+	while (flag)
+	{
+		////设置剪辑区域
+		
+		clearcliprgn();
+		ExMessage msg;
+		peekmessage(&msg);
+		switch (msg.message)
+		{
+		case WM_KEYDOWN:
+			if (msg.vkcode == VK_RETURN) {
+				flag = 0;
+				break;
+			}//两条语句，请把大括号带上！！！
+			
+
+			if (GetAsyncKeyState(VK_UP)) {
+				UpperLeft.y -= 1;
+				LowerRight.y -= 1;
+			}
+			if (GetAsyncKeyState(VK_DOWN)) {
+				UpperLeft.y += 1;
+				LowerRight.y += 1;
+			}
+			if (GetAsyncKeyState(VK_LEFT)) {
+				UpperLeft.x -= 1;
+				LowerRight.x -= 1;
+			}
+			if (GetAsyncKeyState(VK_RIGHT)) {
+				UpperLeft.x += 1;
+				LowerRight.x += 1;
+			}
+		}
+
 	rectangle(UpperLeft.x, UpperLeft.y, LowerRight.x, LowerRight.y);
 	Window::flushDraw();
-	DeleteObject(rect);
+	}
+	/*rectangle(UpperLeft.x, UpperLeft.y, LowerRight.x, LowerRight.y);
+	Window::flushDraw();*/
+	/*DeleteObject(rect);*/
 }
 
 Rect::Rect()
